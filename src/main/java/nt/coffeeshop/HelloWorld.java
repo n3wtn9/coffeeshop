4a404a43c6d48717ead589f6ae17c44dbd5504e0
Newton Truong <n3wtn9@gmail.com>
2017-10-18 21:58:16 -0700

    more spark progress

--- src/main/java/nt/coffeeshop/HelloWorld.java
+++ src/main/java/nt/coffeeshop/HelloWorld.java
@@ -4,6 +4,17 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         get("/hello", (req, res) -> "Hello World");
+
+        get("/menu", (req, res) ->
+                "1. espresso " +
+                "2. americano " +
+                "3. latte");
+
+        post("/order", (req, res) -> {
+            res.body("order comming...");
+            return "done";
+        });
     }
 }
